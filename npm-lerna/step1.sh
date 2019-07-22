13e60dd3400f8799f98473b535aa79a59b7d3018
Andreas Augustin <dev@andreas-augustin.org>
2019-07-22 10:52:04 +0200

    (#15) mv assets to assets/ folder

--- npm-lerna/step1.sh
+++ npm-lerna/step1.sh
@@ -2,7 +2,7 @@
 
 DIR=$(dirname "$0")
 
-kubectl apply -f "${DIR}/assets/infrastructure/gitea.pod.yaml"
-kubectl apply -f "${DIR}/assets/infrastructure/verdaccio.pod.yaml"
-kubectl apply -f "${DIR}/assets/infrastructure/gitea.svc.yaml"
-kubectl apply -f "${DIR}/assets/infrastructure/verdaccio.svc.yaml"
+kubectl apply -f "${DIR}/assets/gitea.pod.yaml"
+kubectl apply -f "${DIR}/assets/verdaccio.pod.yaml"
+kubectl apply -f "${DIR}/assets/gitea.svc.yaml"
+kubectl apply -f "${DIR}/assets/verdaccio.svc.yaml"
