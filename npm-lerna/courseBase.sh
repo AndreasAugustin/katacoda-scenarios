1cc4e6b7fdb6f2a4f0b0c3b936d6839dd7cc1203
Andreas Augustin <dev@andreas-augustin.org>
2019-07-22 11:55:30 +0200

    (#15) move assets

--- npm-lerna/courseBase.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply -f assets/gitea.pod.yaml
-kubectl apply -f assets/verdaccio.pod.yaml
-kubectl apply -f assets/gitea.svc.yaml
-kubectl apply -f assets/verdaccio.svc.yaml
