85b6e394180a4788a0900386d05ad0c1d6b7daed
Andreas Augustin <dev@andreas-augustin.org>
2019-07-22 12:05:51 +0200

    (#15) move assets

--- npm-lerna/assets/bin/courseBase.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=$(dirname $0)
-
-kubectl apply -f $DIR/../etc/gitea.pod.yaml
-kubectl apply -f $DIR/../etc/verdaccio.pod.yaml
-kubectl apply -f $DIR/../etc/gitea.svc.yaml
-kubectl apply -f $DIR/../etc/verdaccio.svc.yaml
