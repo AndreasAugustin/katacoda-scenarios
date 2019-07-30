33fb7f21d0c9f4e9b549686c0532a1f3e357b14e
Andreas Augustin <dev@andreas-augustin.org>
2019-07-30 23:13:52 +0200

    (#11) init gitea done, ready for test :rocket

--- npm-lerna/assets/setup_env.sh
+++ npm-lerna/assets/setup_env.sh
@@ -10,6 +10,7 @@ DOCKER_COMPOSE_CMD="docker-compose -f ${DIR}/docker-compose.yml"
 USERNAME=$(git config --global user.name) || "john.doe"
 EMAIL=$(git config --global user.email) || "john.doe@random.me"
 PASSWORD=secret
+GITEA_URL="http://localhost:30002"
 
 echo "username set to: ${USERNAME}"
 echo "email set to: ${EMAIL}"
@@ -18,7 +19,7 @@ echo "email set to: ${EMAIL}"
 #   Comands
 #####################
 echo "-------------- starting docker container"
-${DOCKER_COMPOSE_CMD} up -d
+#${DOCKER_COMPOSE_CMD} up -d
 
 # Prepare git
 echo "-------------- prepare git"
@@ -28,15 +29,18 @@ git config --global --replace-all user.email "${EMAIL}"
 
 # Prepare Gitea
 echo "-------------- prepare gitea environment"
-# Little hack to install
-${DOCKER_COMPOSE_CMD} exec -u git gitea gitea admin create-user --name="${USERNAME}" --password="${PASSWORD}" --email "${EMAIL}"
-mkdir -p "${GIT_DIR}"
 
-# touch README.md
+#${DOCKER_COMPOSE_CMD} exec -u git gitea gitea admin create-user --name="${USERNAME}" --password="${PASSWORD}" --email "${EMAIL}"
+
+curl -X POST \
+    "${GITEA_URL}/api/v1/user/repos" \
+    -H "accept: application/json" \
+    -H "Content-Type: application/json" \
+    -d "{ \"auto_init\": true, \"description\": \"Lerna tutorial\", \"gitignores\": \"Node\", \"license\": \"MIT\", \"name\": \"npm-lerna\", \"private\": false, \"readme\": \"Default\"}" \
+    -u "${USERNAME}:${PASSWORD}"
+
+# init local repo
+mkdir -p "${GIT_DIR}"
 pushd "${GIT_DIR}" || exit
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git remote add origin http://localhost:30002/my-user/tmp.git
-# git push -u origin master
+git clone "${GITEA_URL}/${USERNAME}"/npm-lerna.git
 popd || exit
