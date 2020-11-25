5b419496dee18250aa3fe28f5e71c04bd187c286
AndreasAugustin <dev@andreas-augustin.org>
2020-11-25 10:07:42 +0100

    fix(): remove mkdir part

--- tsdevops/npm-lerna/assets/setup_env.sh
+++ tsdevops/npm-lerna/assets/setup_env.sh
@@ -52,7 +52,6 @@ curl -X POST \
     -u "${USERNAME}:${PASSWORD}"
 
 # init local repo
-mkdir -p "${GIT_DIR}/${REPO_NAME}"
 pushd "${GIT_DIR}" || exit
 git clone "${GITEA_URL}/${USERNAME}/${REPO_NAME}.git"
 popd || exit
